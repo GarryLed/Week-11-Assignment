119e46627ca77e80f62a7bf61b95f1a4684b950a
GarryLed <s00256356@atu.ie>
2024-11-26 22:20:30 +0000

    Working on async function: fetchPlayerProfiles

--- script.js
+++ script.js
@@ -49,5 +49,48 @@ async function fetchPlayerTitles(title) {
         }
 }
 
+// add an event listener 
+document.getElementById('get-player-profiles').addEventListener('click', fetchPlayerProfiles)
+
+/**
+ * Fetch movie data and display each movie in a card format.
+ * Uses async/await for a cleaner asynchronous flow.
+ */
+async function fetchPlayerProfiles() {
+    try {
+      const response = await fetch(`https://api.chess.com/pub/player/garyleddy`);
+      const data = await response.json(); // Parses the response as JSON
+     console.log(data);
+      // Iterate over each movie object and create a card for it
+      //data.forEach(player => {
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+  
+        const h1 = document.createElement('h1');
+        h1.textContent = data.username;
+  
+        const p = document.createElement('p');
+        p.textContent = ` Membership: ${data.title}`;
+
+        const p2 = document.createElement('p');
+        p.textContent = data.status;
+  
+       const container = document.getElementById('get-player-profiles');
+  
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+        card.appendChild(p2);
+     // });
+    } catch (error) {
+      // Display an error message if the fetch fails
+      const errorMessage = document.createElement('div');
+      errorMessage.textContent = `Gah, it's not working! Error: ${error.message}`;
+      container.appendChild(errorMessage);
+    }
+  }
+
 // testing the API 
 //fetchPlayerTitles(`GM`);
+
+//fetchPlayerProfiles('garryleddy');
